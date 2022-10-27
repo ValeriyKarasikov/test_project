fba39b52cdb2a3f233388c00978ade99c3498ddf
valeriy.karasikov@gmail.com <valeriy.karasikov@gmail.com>
2022-10-27 12:03:38 +0300

    2022.10.27(gerasimov)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+void kek()
+{
+  std::cout << "hello there!" << std::endl;
+}
+
 int main()
 {
+  ::kek();
   std::cout << "group #42" << std::endl;
   std::cin.get();
   return 0;
